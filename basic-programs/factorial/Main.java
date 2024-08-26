1f792f875a08bb52e1bbf84dcd57baf154734c0f
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2024-08-26 19:23:04 +0530

    add story

--- basic-programs/factorial/Main.java
+++ basic-programs/factorial/Main.java
@@ -3,12 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+
+        // Story intro
+        System.out.println("Afzal, a bright schoolboy, was learning about factorials in his math class.");
+        System.out.println("His teacher gave him an exercise to find the factorial of a number, and Afzal decided to solve it using a program.");
+
+        // Prompting user input
+        System.out.print("Enter a number to calculate its factorial: ");
         int num = scanner.nextInt();
+
         long factorial = 1;
+        // Calculating factorial
         for (int i = num; i > 0; i--) {
             factorial = factorial * i;
+            // Story detail about the process
+            System.out.println("Afzal is multiplying " + factorial + " by " + i + " to find the factorial.");
         }
-        System.out.println("Factorial of " + num + " is: " + factorial);
+
+        // Displaying result
+        System.out.println("The factorial of " + num + " is: " + factorial);
+        System.out.println("Afzal was pleased with his result and felt more confident about his math skills after solving the exercise.");
+        System.out.println("His teacher congratulated him and said, 'Great job, Afzal! You've successfully calculated the factorial!'");
     }
 }
