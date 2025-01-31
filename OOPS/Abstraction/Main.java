1bde07e7c1da7943cf0cb9ee2bb72ecaa230b6d4
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2025-01-31 08:31:40 +0530

    OOPS Pillar: Abstraction

---
+++ OOPS/Abstraction/Main.java
@@ -0,0 +1,23 @@
+package Abstraction;
+class Person {
+    private String name;
+    private int age;
+
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public Boolean canVote(){
+        return age >=18;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args){
+        Person afzal = new Person("Mohammed Afjal", 20);
+
+        System.out.println(afzal.canVote()); // only this shown to the user the canVote not the implementation of this method
+    }
+}
