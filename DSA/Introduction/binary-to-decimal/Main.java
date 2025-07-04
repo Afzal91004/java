057edfd9d0242efe834e0d81690e5e863c791a50
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2025-07-04 12:16:55 +0530

    binary to decimal

---
+++ DSA/Introduction/binary-to-decimal/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Binary to Decimal Converter");
+        System.out.print("Enter a binary number: ");
+        
+        String bin = sc.next();
+
+          if (!bin.matches("[01]+")) {
+            System.out.println("Invalid input! Please enter a binary number (only 0 and 1).");
+            return;
+        }
+
+
+        int decimalValue = 0;
+        int length = bin.length();
+
+        for(int i = 1; i<length; i++){
+            char bit = bin.charAt(length - 1 - i);
+            // System.out.println(bit);
+            if(bit == '1'){
+                decimalValue += Math.pow(2, i);
+            } else if (bit != '0') {
+                System.out.println("Invalid binary number.");
+                return;
+            }
+        }
+
+        System.out.println("Decimal value: " + decimalValue);
+
+    }
+}
