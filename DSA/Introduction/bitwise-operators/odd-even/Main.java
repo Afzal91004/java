3de14dbcca3296fe499dd254fd68c074e9f5d6c6
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2025-07-09 10:04:40 +0530

    implementation of bitwise & operator

---
+++ DSA/Introduction/bitwise-operators/odd-even/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Odd or Even using bitwise operator");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if(number < 0) {
+            System.out.println("Please enter a non-negative integer.");
+        } else {
+            if ((number & 1) == 1) {
+                System.out.println("The number " + number + " is Odd.");
+            } else {
+                System.out.println("The number " + number + " is Even.");
+            }
+        }
+
+        sc.close();
+    }
+}
