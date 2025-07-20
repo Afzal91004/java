a32b82a592964f87f80da7d6a0b3685b94ee21b2
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2025-07-20 20:43:11 +0530

    bitwise operator -  exor implementation program

--- DSA/Introduction/bitwise-operators/exor-operator/Swap.java
+++ DSA/Introduction/bitwise-operators/exor-operator/Swap.java
@@ -1,20 +1,25 @@
-import java.util.Scanner;
+import java.util.Scanner;;
 
 public class Swap {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter first number(a): ");
+        System.out.println("******************Swap Two Numbers Using XOR Operator******************");
+        System.out.print("Enter the first number (a): ");
         int a = sc.nextInt();
-        System.out.print("Enter second number(b): ");
+        System.out.print("Enter the second number (b): ");
         int b = sc.nextInt();
 
-        System.out.println("Before Swap: a= " + a + " b = " + b);
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
         // Swapping using XOR operator
-        a = a ^ b; 
-        b = a ^ b; 
-        a = a ^ b;
-        System.out.println("After Swap: a= " + a + " b = " + b);
+        a = a ^ b; // Step 1: a now holds the XOR of a and b 
+        // above line: a = a ^ b = a'
+        b = a ^ b; // Step 2: b now holds the original value of a
+        // above line: b = a' ^ b = a' ^ (a ^ b) = a
+        a = a ^ b; // Step 3: a now holds the original value of b
+        // above line: a = a' ^ b 
+        // but b = a
+        // so a = a' ^ a = a ^ b ^ a = b
+        System.out.println("After swapping: a = " + a + ", b = " + b);
         sc.close();
     }
 }
