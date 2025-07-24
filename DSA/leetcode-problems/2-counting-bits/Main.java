d331709cf010864862719bbb21bb09771ff53179
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2025-07-24 05:54:29 +0530

    counting bits implementation

---
+++ DSA/leetcode-problems/2-counting-bits/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Decimal Number: ");
+        int n = sc.nextInt();
+        int[] result = new int[n + 1];
+
+        for(int i = 0; i <= n; i++) {
+            int count = 0;
+            int num = i;
+
+            while(num != 0) {
+                if((num & 1) == 1)
+                    count++;
+                num = num >> 1;
+            }
+            result[i] = count;
+        }
+
+        System.out.println("counting bits: "+ Arrays.toString(result));
+
+    }
+}
+// This program counts the number of 1's in the binary representation of each number from 0 to n.
+// It uses bitwise operations to determine the count of 1's for each number.
+// The result is stored in an array and printed at the end.
+// The time complexity is O(n log n) due to the while loop for each number
+// and the space complexity is O(n) for storing the results in an array.
+// The program prompts the user to enter a decimal number and then calculates the number of bits for
+// each number from 0 to that input number.
+// The program uses bitwise operations to efficiently count the bits.   
+// The program is designed to be run in a console environment where the user can input a number.
+// It is a simple implementation of counting bits using bitwise operations in Java.
