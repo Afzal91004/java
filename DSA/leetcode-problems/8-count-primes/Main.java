063bcf0eb98f65a26a94951573cc08447d8b736e
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2025-07-28 07:26:42 +0530

    count primes

---
+++ DSA/leetcode-problems/8-count-primes/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Input must be a non-negative integer.");
+            return;
+        }
+
+        int primeCount = countPrimes(n);
+        System.out.println("Number of prime numbers less than " + n + ": " + primeCount);
+    }
+
+    public static int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
