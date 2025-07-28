9829f2f45f755bc53421c24b9634ec60522b171b
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2025-07-28 20:16:33 +0530

    fixes

---
+++ DSA/Introduction/basics-of-recursion/recursion/Factorial.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        
+        System.out.println("Factorial Calculation Program using Recursion");
+        
+        while(true) {
+            System.out.print("Enter a number to calculate its factorial (or -1 to exit): ");
+            num = sc.nextInt();
+            if(num >= 0) {
+                break;
+            }
+            System.out.println("Factorial is not defined for negative numbers. \nPlease enter a non-negative integer.");
+        }
+
+        System.out.println("Calculating factorial of " + num + " using recursion...");
+        System.out.println("The factorial of " + num + " is: " + factorial(num));
+    }
+
+    public static long factorial(int n) {
+        if(n == 0 || n == 1) return 1;
+        return n * factorial(n - 1);
+    }
+}
