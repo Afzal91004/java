a6841751edee6e2d88cacdd28fc01b76377ad308
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2025-07-28 15:58:59 +0530

    count primes fixes

--- DSA/leetcode-problems/8-count-primes/Main.java
+++ DSA/leetcode-problems/8-count-primes/Main.java
@@ -27,7 +27,6 @@ public class Main {
     }
 
     public static boolean isPrime(int num) {
-        if (num < 2) return false;
         for (int i = 2; i <= (int)Math.sqrt(num); i++) {
             if (num % i == 0) return false;
         }
